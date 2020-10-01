cd40cc01497e041a211e88fccc6fc316a5fca4f9
Igor Kutsan <userdevel1405@gmail.com>
2020-10-01 15:16:34 +0300

    new design

--- components/Client/Header/Header.vue
+++ components/Client/Header/Header.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="parentHeader">
-    <Clock />
-  </div>
+  <b-container>
+    <b-row>
+      <b-col class="header_column" md="4" />
+      <b-col class="header_column" md="4">
+        <Clock />
+      </b-col>
+      <b-col class="header_column" md="4" />
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -13,9 +19,11 @@ export default {
 </script>
 
 <style scoped>
-  .parentHeader {
-    margin: 15px 0;
-    padding: 10px 0;
-    text-align: center;
+  .header_column {
+    padding: 5px 10px;
+    border-left: 1px solid rgba(255, 255, 255, .12);
+  }
+  .header_column:last-child {
+    border-right: 1px solid rgba(255, 255, 255, .12);
   }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,14 @@
 <template>
   <div>
     <header>
-      <b-container>
+      <b-container fluid>
         <Header />
       </b-container>
     </header>
-    <!--    <nav><Menu /></nav>-->
+    <nav><Menu /></nav>
     <Nuxt />
   </div>
 </template>
-
-<style>
-html, body {
-    height: 100vh;
-}
-
-body {
-  background-image: url("~static/img/time.jpg");
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-}
-
-header {
-  padding-top: 1px;
-}
-</style>
 
 <script>
 import Header from '@/components/Client/Header/Header'
